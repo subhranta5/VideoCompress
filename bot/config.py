3635eb934e87f1765d2ff7e570b451571270583b
subhranta5 <81546243+subhranta5@users.noreply.github.com>
2021-07-26 11:55:49 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SESSION_NAME = get_config("SESSION_NAME", "CompressorBot")
     
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "1873967215:AAEnXwKYh1NPm9sOZI9i5kysEpydQLrIbj8")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "1873967215:AAHj9ZjXV7J9yodIuqKZ3dLhuXhZmwQDG-I")
     # The Telegram API things
     APP_ID = int(get_config("APP_ID", 6252680))
     API_HASH = get_config("API_HASH", "2ae08a73819c57a93f5381d05746e511")
